04a085fa4f8e48e7ec9ad7cd26a235395f48acce
ibnorwa <hani.talalwah@gmail.com>
2018-09-21 14:22:29 +0300

    new layout changes
    just layout changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,24 @@
 import { UserService } from './../../services/user.service';
 import { FormsModule } from '@angular/forms';
-import {Component, OnInit, NgModule, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, NgModule, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  @Output () event = new EventEmitter<boolean>();
-  constructor(private auth:UserService) { }
+  @Output() event = new EventEmitter<boolean>();
+  constructor(private auth: UserService) { }
   email = '';
   password = '';
-  reg= false;
+  reg = false;
   login() {
 
     this.auth.login(this.email, this.password);
     this.event.emit(false);
   }
 
-  logout(){
+  logout() {
     this.auth.logout();
   }
   register() {
